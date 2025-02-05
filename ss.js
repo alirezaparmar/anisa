a9df1af649b9e737691998f9fbd0ceed26f2cd80
Alireza parmar <Alirezaparmar@gmail.com>
2025-02-05 10:33:55 +0330

    p

---
+++ ss.js
@@ -0,0 +1,49 @@
+
+// 1
+var firstName = "Alireza";
+var lastName = "Parmar";
+var age = 23;
+var city = "Bandarabbas";
+
+console.log(`=> my name is ${firstName} ${lastName} `);
+console.log(`=> I have ${age}y old  `);
+console.log(`=>  I'm living in ${city} `);
+
+
+
+// 2 
+//  میکند شناور استفاده ممیز  اعداد  برای  IEEE 754
+//  ترین نزدیک به را ان میکند سعی اسکریپت جاوا میکنید وارد را 999999999999 عدد وقتی دلیل همین به 
+//  شود تبدیل 100000000000 به 9999999999 عدد که میشود موجب تبدیل این کند تبدیل دهد نمایش میتواند که عددی 
+
+
+
+// 3
+
+//  داده نمایش دقیق طور به نمیتواند اعداد برخی و دارد محدودی دقت استاندارد این میکند استاندارد IEEE 754 شناور ممیز اعداد برای folating-point numbers استفاده از جاوااسکریپت است کامپیوتر در اعشاری اعداد سازی ذخیره نحوه به مربوط مسئله  این 
+//  میشود داده نمایش این خروجی در نتیجه و شوند داده نمایش دقیق طور به نمیتوانند شدن جمع هنگام اعداد این دلیل  همین به  شوند 
+
+
+// 4
+
+var alireza = {
+    firstName: "Alireza",
+    lastName: "Parmar",
+    age: 23,
+    city: "Bandarabbas",
+    family: {
+        father: "Hedayat",
+        mother: "Soheyla",
+        siblings: ["Sanaz", "Hamidreza"]
+    },
+    friends: ["Ramin", "Yousef", "Ali", "Milad"]
+};
+
+console.log(alireza);
+
+
+// 4
+
+//  باشد  null یک عنوان    type of null  خروجی 
+بازمیگردد باز  جاوااسکریپت  طراحی  زمان در تاریخی اشتباه 
+                                                                      
